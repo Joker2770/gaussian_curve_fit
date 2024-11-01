0ed83f824265a37a231f0f5e384ff7bb09b6c7f4
Joker2770 <1214220480@qq.com>
2024-11-01 11:23:12 +0800

    docs: some adjustments for header :bookmark:

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+/*
+A no_std library for gaussian curve coefficents calculation.
+Copyright (C) 2024  joker2770
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #![no_std]
 
 pub mod gaussian_curve_fit;
